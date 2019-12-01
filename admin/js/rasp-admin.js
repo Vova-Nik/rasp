551767f848e04ab62447db48b6b64b6cbe0d66c6
Vova-Nik <vmaseich@gmail.com>
2019-12-01 21:39:15 +0200

    admin editing buttons described

--- admin/js/rasp-admin.js
+++ admin/js/rasp-admin.js
@@ -41,41 +41,15 @@
 
 		let rasp = await read_from_db();
 		rasp = JSON.parse(rasp);
-		$('body').append(`<div>${rasp}</div>`);
 
-		let save_res = await save_to_db();
-		console.log(save_res);
+		//$('body').append(`<div>${rasp}</div>`);
 
-		$(".admin-grid-container").append(`
-			<div  class="admin-grid-container-div-th">Id</div>
-			<div  class="admin-grid-container-div-th">Day</div>
-			<div  class="admin-grid-container-div-th">Time</div>
-			<div  class="admin-grid-container-div-th">Place</div>
-			<div  class="admin-grid-container-div-th">Name</div>
-			<div  class="admin-grid-container-div-th">Display</div>
-			`);  //Header of table
+		//let save_res = await save_to_db();
+		console.log(rasp);
 
-		rasp.forEach(function (item, i) {
-			$(".admin-grid-container").append(`<div class="admin-grid-container-div agcd-row${i}"> ${item.id}</div>`);
-			$(".admin-grid-container").append(`<div class="admin-grid-container-div agcd-row${i}"> ${item.event_day_of_week} </div>`);
-			$(".admin-grid-container").append(`<div class="admin-grid-container-div agcd-row${i}"> ${item.event_begin_time} </div>`);
-			$(".admin-grid-container").append(`<div class="admin-grid-container-div agcd-row${i}"> ${item.event_place} </div>`);
-			$(".admin-grid-container").append(`<div class="admin-grid-container-div agcd-row${i}"> ${item.event_name} </div>`);
-			$(".admin-grid-container").append(`<div class="admin-grid-container-div agcd-row${i}"> ${item.event_show} </div>`);
-		});
-
-		let raspEdit = new RaspEdit(); //rasp[cl]
-
-		$(".admin-grid-container-div").dblclick(function (element) {
-			element.srcElement.classList.forEach(cl => {
-				if (cl.indexOf('agcd-row') > -1) {
-					cl = parseInt(cl.substring(8));
-					//console.log(rasp[cl]);
-					//let raspEdit = new RaspEdit(rasp[cl]);
-					raspEdit.editEvent(rasp[cl]);
-				}
-			});
-		});
+		let raspTable = new RaspTable(rasp);
+		//raspTable.init(rasp);
+		raspTable.addRow(rasp[1]);
 
 	} // end of main()
 
@@ -92,94 +66,99 @@
 	}
 
 
-	class RaspEdit {
+	class RaspRecord {
 
-		constructor(){
-			//this.rasp = rasp_array;
-			this.is_activated = false;
-			this.wp_client_area = document.querySelector("#wpbody-content");
-
-			this.edit_conteiner = document.createElement("div"); //
-			this.edit_conteiner.className = "edit-container";
-			this.edit_conteiner.style.display = "none";
-
-			this.wp_client_area.insertAdjacentElement("beforeBegin", this.edit_conteiner);
-			this.edit_area = document.createElement("div");
-			this.edit_area.className = "edit-area";
-			this.edit_conteiner.appendChild(this.edit_area);
-			//console.log(this.edit_conteiner);
-			this.edit_area.innerHTML = `
-			<form id = "rasp-form">
-				<ul class="flex-outer">
-					<li>
-						<label for="form-time">Event time*</label>
-						<input type="time" id="form-time" placeholder="Time of beginning">
-					</li>
-					<li>
-						<label for="form-name">Event Name*</label>
-						<input type="text" id="form-name" placeholder="Enter event name">
-					</li>
-					<li>
-						<label for="form-description">Description</label>
-						<input type="text" id="form-description" placeholder="Event description">
-					</li>
-					<li>
-						<label for="form-url">Event URL</label>
-						<input type="url" id="form-url" placeholder="Event url">
-					</li>
-					<li>
-						<label for="form-day">Dday of week (0..6)*</label>
-						<input type="number" id="form-day" placeholder="Event day of week">
-					</li>
-
-					<li>
-					<div id = "form_btn_copy" class = 'form-btn'>Change</div>
-					<div id = "form_btn_save" class = 'form-btn'>Save as copy</div>
-					</li>
-				</ul>
-			</form>
-			`
-		}
-
-
-		is_Activated(){
-			return this.is_activated;
+		constructor(record) {
+			this.rec = record;
 		}
 
 		editEvent(event_to_edit) {
-			if(this.is_activated)
-				return;
-			this.is_activated = true;
-			this.table_container = document.querySelector(".admin-grid-container");
-			this.table_container.style.opacity = ".8";
-			this.edit_conteiner.style.display = "";
-			console.log(event_to_edit);
-
-			document.querySelector("#form-time").innerHTML = event_to_edit.event_begin_time;
-
-			//console.log(this.rasp[parseInt(event_to_edit)]);
 			console.log(this.rasp[1]);
-
 			this.btn_copy = document.querySelector("#form_btn_copy");
 			this.btn_save = document.querySelector("#form_btn_save");
-
-
-
-			
-
-			this.btn_copy.onclick = (function()
-				{alert("button copy");}
+			this.btn_copy.onclick = (function () { alert("button copy"); }
 			);
-			this.btn_save.onclick = (function()
-				{alert("button save");}
+			this.btn_save.onclick = (function () { alert("button save"); }
 			);
+		}
 
-
-
-			event_to_edit = null;
-			return event_to_edit;
+		to_string() {
+			return JSON.stringify(this.rasp);
 		}
 	}
+
+	class RaspTable {
+
+	constructor(rasp_obj) {
+			this.rows_number = 0;
+			this.rasp = rasp_obj;
+
+			$(".admin-grid-container").append(`
+			<div  class="admin-grid-container-div-th">Copy</div>
+			<div  class="admin-grid-container-div-th">Id</div>
+			<div  class="admin-grid-container-div-th">Day</div>
+			<div  class="admin-grid-container-div-th">Time</div>
+			<div  class="admin-grid-container-div-th">Place</div>
+			<div  class="admin-grid-container-div-th">Name</div>
+			<div  class="admin-grid-container-div-th">Display</div>
+			<div  class="admin-grid-container-div-th">Del</div>
+			`);  //Header of table
+
+			this.rasp.forEach((item) => {
+				this.addRow(item);
+			});
+
+			$(".admin-grid-container").append(`
+			<div contenteditable="true" class="admin-grid-container-div-ed">--</div>
+			<div contenteditable="true" class="admin-grid-container-div-ed">--</div>
+			<div contenteditable="true" class="admin-grid-container-div-ed">--</div>
+			<div contenteditable="true" class="admin-grid-container-div-ed">--</div>
+			<div contenteditable="true" class="admin-grid-container-div-ed">--</div>
+			<div contenteditable="true" class="admin-grid-container-div-ed">--</div>
+			<div contenteditable="true" class="admin-grid-container-div-ed">--</div>
+			<div contenteditable="true" class="admin-grid-container-div-ed">--</div>
+			`);
+		}
+
+		copy_btn_processing (eventObject) {
+			console.log(eventObject.data)
+		};
+
+		del_btn_processing (eventObject) {
+			console.log(eventObject.data)
+		};
+
+
+		addRow(row) {
+			
+			console.log(this.rows_number);
+
+			$(".admin-grid-container").append(`<div  class="btn_copy agcd-row${this.rows_number} copy-btn${this.rows_number}"> copy </div>`);
+
+			$(".admin-grid-container").append(`<div contenteditable="true" class="admin-grid-container-div agcd-row${this.rows_number}"> ${row.id}</div>`);
+			$(".admin-grid-container").append(`<div contenteditable="true" class="admin-grid-container-div agcd-row${this.rows_number}"> ${row.event_day_of_week} </div>`);
+			$(".admin-grid-container").append(`<div contenteditable="true" class="admin-grid-container-div agcd-row${this.rows_number}"> ${row.event_begin_time} </div>`);
+			$(".admin-grid-container").append(`<div contenteditable="true" class="admin-grid-container-div agcd-row${this.rows_number}"> ${row.event_place} </div>`);
+			$(".admin-grid-container").append(`<div contenteditable="true" class="admin-grid-container-div agcd-row${this.rows_number}"> ${row.event_name} </div>`);
+			$(".admin-grid-container").append(`<div contenteditable="true" class="admin-grid-container-div agcd-row${this.rows_number}"> ${row.event_show} </div>`);
+			$(".admin-grid-container").append(`<div class="btn_del agcd-row${this.rows_number} del-btn${this.rows_number}"> del </div>`);
+
+			$(`.copy-btn${this.rows_number}`).on( "click",  {num: this.rows_number, act: "copy"},  this.copy_btn_processing); // , {num: this.rows_number});
+			$(`.del-btn${this.rows_number}`).on( "click",  {num: this.rows_number, act: "del"},  this.del_btn_processing);
+			
+
+			this.rows_number++;
+
+		}
+
+		get number_of_rows() {
+			return this.rows_number;
+		}
+
+	}
+
+
+
 
 	/*
 	event_begin_time: "19:00:00"
